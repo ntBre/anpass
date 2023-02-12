7bddea7eccb4ae9377d338273952b58f795bba6f
Brent Westbrook <brentrwestbrook@gmail.com>
2023-02-12 16:15:25 -0600

    update main.rs to use `run` and print results

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rust_anpass::{Anpass, Bias};
+use rust_anpass::Anpass;
 
 fn main() {
     let args: Vec<_> = std::env::args().collect();
@@ -7,18 +7,10 @@ fn main() {
         Some(s) => Anpass::load_file(s),
         None => Anpass::load(std::io::stdin()),
     };
-    // perform the initial fitting
-    let (coeffs, _) = anpass.fit();
-    // find the stationary point
-    let (x, _) = anpass.newton(&coeffs).unwrap();
-    // determine energy at the stationary point
-    let e = anpass.eval(&x, &coeffs);
-    // bias the displacements and energies to the new stationary point
-    let anpass = anpass.bias(&Bias { disp: x, energy: e });
-    // perform the refitting
-    let (coeffs, _) = anpass.fit();
-    // make and write the fort.9903 file expected by intder
-    let f9903 = anpass.make9903(&coeffs);
+    let (f9903, bias, res, kind) = anpass.run().unwrap();
+    println!("bias: {bias}");
+    println!("Sum of squared residuals: {res:12.6e}");
+    println!("stationary point is a {kind}");
     let filename = "fort.9903";
     let mut f = match std::fs::File::create(filename) {
         Ok(f) => f,
